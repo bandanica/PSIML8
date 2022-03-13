a1d2a969ee0f567681227ff62e288dca3a90d827
bandanica <65416715+bandanica@users.noreply.github.com>
2022-03-13 09:47:39 +0100

    find number of json files in set (task A)

--- Homework/rock_my_world/main.py
+++ Homework/rock_my_world/main.py
@@ -8,10 +8,18 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-def countJsonFiles(dataset):
-    return dataset
+import glob
+import numpy as np
+
+def countJsonFiles(root_path):
+    json_files = glob.glob(root_path + "/**/*.json", recursive=True)
+    return json_files
 
 if __name__ == "__main__":
     root_path = input()
-    dataset = root_path.split("/")[-1]
-    x = countJsonFiles(dataset)
+    #dataset = root_path.split("/")[-1]
+    print(np.size(countJsonFiles(root_path)))
+    print()
+    print()
+    print()
+    print()
